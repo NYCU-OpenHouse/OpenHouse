70d59256c85fdefd542922d87c7f451870567f27
Billy Su <g4691821@gmail.com>
2018-09-21 10:33:20 +0800

    Delete RDSS for normal user

--- rdss/public_urls.py
+++ rdss/public_urls.py
@@ -7,11 +7,16 @@ urlpatterns = [
     # url(r'^$', 'oh2016_dj.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$',views.RDSSPublicIndex,name="rdss_index"),
-    url(r'^seminar$',views.SeminarPublic,name="seminar_public"),
-    url(r'^jobfair$',views.JobfairPublic,name="jobfair_public"),
-    url(r'^jobs$',views.ListJobs,name="rdss_jobs"),
-    url(r'^querypts$',views.QueryPoints,name="rdss_querypts"),
+    # The following url comment out, because there won't be any rdss event
+    # in the future.
+
+    # Modified by Billy Su. Sep. 21 2018
+
+    # url(r'^$',views.RDSSPublicIndex,name="rdss_index"),
+    # url(r'^seminar$',views.SeminarPublic,name="seminar_public"),
+    # url(r'^jobfair$',views.JobfairPublic,name="jobfair_public"),
+    # url(r'^jobs$',views.ListJobs,name="rdss_jobs"),
+    # url(r'^querypts$',views.QueryPoints,name="rdss_querypts"),
 
 	#export urls are defined in admin.py
 ]
