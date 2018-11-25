3f53d9da9a539f6a9721a85b1e2d36e6b03f8dd4
Billy Su <g4691821@gmail.com>
2018-11-25 22:17:48 +0800

    Setup vote time limit

--- vote/views.py
+++ vote/views.py
@@ -1,19 +1,27 @@
 from django.shortcuts import render
+from django.utils import timezone
 from .models import Participant,Vote, VoteInfo
 from ipware.ip import get_real_ip,get_ip
 from django.db import IntegrityError
 import datetime
+
 def vote(request):
+    start_time = datetime.datetime(2018, 11, 26, 0, 0, tzinfo=timezone.get_current_timezone())
+    end_time = datetime.datetime(2018, 11, 30, 23, 59, 59, tzinfo=timezone.get_current_timezone())
+    now = timezone.now()
     if request.POST:
-        ip = get_ip(request)
-        if ip is not None:
-            participant_id = request.POST['participant']
-            participant = Participant.objects.get(id=participant_id)
-            vote = Vote(participant=participant,ip=ip,date=datetime.date.today())
-            try:
-                vote.save()
-            except IntegrityError as e:
-                error_msg = "每人一天限投一票"
+        if start_time > now or now > end_time:
+            error_msg = "投票尚未開始"
+        else:
+            ip = get_ip(request)
+            if ip is not None:
+                participant_id = request.POST['participant']
+                participant = Participant.objects.get(id=participant_id)
+                vote = Vote(participant=participant,ip=ip,date=datetime.date.today())
+                try:
+                    vote.save()
+                except IntegrityError as e:
+                    error_msg = "每人一天限投一票"
     participants = Participant.objects.all()
     return render(request,'vote.html',locals())
                 
