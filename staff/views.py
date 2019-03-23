3d26f2296bc4dc580a80c444779cf4b2de2e4334
Billy Su <g4691821@gmail.com>
2019-03-23 17:29:08 +0800

    Fix PEP8

--- staff/views.py
+++ staff/views.py
@@ -4,15 +4,15 @@ import staff.forms
 # Create your views here.
 
 def StaffCreation(request):
-	if request.POST:
-		form = staff.forms.StaffCreationForm(request.POST,request.FILES)
-		if form.is_valid():
-			user=form.save()
-			return render(request,'success.html')
-		else:
-			print(form.errors)
-			#messages.error(request, ("The user could not be created due to errors.") )
-			return render(request,'staff_form.html',{'form':form})
+    if request.POST:
+        form = staff.forms.StaffCreationForm(request.POST,request.FILES)
+        if form.is_valid():
+            user=form.save()
+            return render(request,'success.html')
+        else:
+            print(form.errors)
+            #messages.error(request, ("The user could not be created due to errors.") )
+            return render(request,'staff_form.html',{'form':form})
 
-	form = staff.forms.StaffCreationForm
-	return render(request,'staff_form.html',{'form':form})
+    form = staff.forms.StaffCreationForm
+    return render(request,'staff_form.html',{'form':form})
