66b8511a57ab16f71a34739cd99e636545f4826d
steven112163 <steven112163@gmail.com>
2020-07-04 19:10:44 +0800

    Implement index page of monograph

--- monograph/urls.py
+++ monograph/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='monograph_index'),
     url(r'^(?P<monograph_id>[0-9].*)/$', views.monograph_detail, name='monograph_detail'),
 ]
--- monograph/views.py
+++ monograph/views.py
@@ -1,10 +1,40 @@
 from django.shortcuts import render
+from django.http import Http404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import MonographInfo, Monograph
 
 
 # Create your views here.
 def index(request):
-    pass
+    """
+    Function displaying latest intro and all monographs
+    """
+    try:
+        mono_info = MonographInfo.objects.latest('updated')
+    except MonographInfo.DoesNotExist:
+        mono_info = None
+
+    paginator = Paginator(Monograph.objects.all(), 1)
+    page_number = request.GET.get('page')
+
+    try:
+        mono = paginator.page(page_number)
+    except PageNotAnInteger:
+        page_number = 1
+        mono = paginator.page(page_number)
+    except EmptyPage:
+        page_number = paginator.num_pages
+        mono = paginator.page(page_number)
+    return render(request, 'monograph_index.html', locals())
 
 
 def monograph_detail(request, monograph_id):
-    pass
+    """
+    Function displaying specific monograph
+    """
+    try:
+        mono = Monograph.objects.get(id=monograph_id)
+    except Monograph.DoesNotExist:
+        raise Http404('Monograph does not exist')
+
+    return render(request, 'monograph_detail.html', locals())
