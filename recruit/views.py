485a87c30f9889e34b7b88bfabb6381e1ceeba27
steven112163 <steven112163@gmail.com>
2021-11-01 15:48:11 +0800

    Update recruit jobfair select

--- recruit/views.py
+++ recruit/views.py
@@ -644,7 +644,7 @@ def jobfair_select_form_gen(request):
         my_signup = RecruitSignup.objects.get(cid=request.user.cid)
         # check the company have signup seminar
         if my_signup.jobfair == 0:
-            error_msg = "貴公司已報名本次校徵活動，但並末填寫就博會攤位。"
+            error_msg = "貴公司已報名本次校徵活動，但並未填寫實體就博會攤位。"
             return render(request, 'recruit/error.html', locals())
     except Exception as e:
         error_msg = "貴公司尚未報名本次活動，請於左方點選「填寫報名資料」"
@@ -655,7 +655,7 @@ def jobfair_select_form_gen(request):
     except Exception as e:
         jobfair_select_time = "選位時間及順序尚未排定，您可以先參考攤位圖"
 
-    place_maps = Files.objects.filter(category='就博會攤位圖')
+    place_maps = Files.objects.filter(category='實體就博會攤位圖')
 
     return render(request, 'recruit/company/jobfair_select.html', locals())
 
