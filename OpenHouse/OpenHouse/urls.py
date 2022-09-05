347d400bfd6ae11bee5671d3fba0ed8c653be2d1
yellow-hank <shps97040608@gmail.com>
2022-09-05 18:56:08 +0800

    feat: OpenHouse OAuth for Company user model
    1. Customize OAuth for Company user model
    2. Staff user model can't use OAuth login
    3. Only Staff member can login Oauth application admin page

--- OpenHouse/OpenHouse/urls.py
+++ OpenHouse/OpenHouse/urls.py
@@ -13,7 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
+from django.urls import include, path
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
@@ -23,6 +24,32 @@ from ckeditor_uploader import views as ckeditor_views
 # for media file
 from django.conf import settings
 from django.conf.urls.static import static
+import oauth2_provider.views as oauth2_views
+
+# OAuth2 provider endpoints
+oauth2_endpoint_views = [
+    path('authorize/', oauth2_views.AuthorizationView.as_view(), name="authorize"),
+    path('token/', oauth2_views.TokenView.as_view(), name="token"),
+    path('revoke-token/', oauth2_views.RevokeTokenView.as_view(), name="revoke-token"),
+]
+
+if settings.DEBUG:
+    # OAuth2 Application Management endpoints
+    oauth2_endpoint_views += [
+        path('applications/', oauth2_views.ApplicationList.as_view(), name="list"),
+        path('applications/register/', oauth2_views.ApplicationRegistration.as_view(), name="register"),
+        path('applications/<pk>/', oauth2_views.ApplicationDetail.as_view(), name="detail"),
+        path('applications/<pk>/delete/', oauth2_views.ApplicationDelete.as_view(), name="delete"),
+        path('applications/<pk>/update/', oauth2_views.ApplicationUpdate.as_view(), name="update"),
+    ]
+
+    # OAuth2 Token Management endpoints
+    oauth2_endpoint_views += [
+        path('authorized-tokens/', oauth2_views.AuthorizedTokensListView.as_view(), name="authorized-token-list"),
+        path('authorized-tokens/<pk>/delete/', oauth2_views.AuthorizedTokenDeleteView.as_view(),
+            name="authorized-token-delete"),
+    ]
+
 
 
 urlpatterns = [
@@ -51,5 +78,6 @@ urlpatterns = [
     url(r'^visit/', include('company_visit.urls')),
     # url(r'^vote/', include('vote.urls')),
     url(r'^monograph/', include('monograph.urls')),
+    path('o/', include((oauth2_endpoint_views, 'oauth2_provider'), namespace="oauth2_provider")),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- OpenHouse/oauth/models.py
+++ OpenHouse/oauth/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
+from company.models import Company
+from oauth2_provider.models import AbstractGrant, AbstractApplication, AbstractAccessToken, AbstractRefreshToken, AbstractIDToken
 # Create your models here.
+
+class CustomAbstractGrant(AbstractGrant):
+    user = models.ForeignKey(
+        Company, on_delete=models.CASCADE, related_name="%(app_label)s_%(class)s"
+    )
+
+class CustomGrant(CustomAbstractGrant):
+    class Meta(CustomAbstractGrant.Meta):
+        swappable = "OAUTH2_PROVIDER_GRANT_MODEL"
+
+
+class CustomAbstractAccessToken(AbstractAccessToken):
+    user = models.ForeignKey(
+        Company,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name="%(app_label)s_%(class)s",
+    )
+
+class CustomAccessToken(CustomAbstractAccessToken):
+    class Meta(CustomAbstractAccessToken.Meta):
+        swappable = "OAUTH2_PROVIDER_ACCESS_TOKEN_MODEL"
+
+
+class CustomAbstractRefreshToken(AbstractRefreshToken):
+    user = models.ForeignKey(
+        Company, on_delete=models.CASCADE, related_name="%(app_label)s_%(class)s"
+    )
+    class Meta:
+        abstract = True
+        unique_together = (
+            "token",
+            "revoked",
+        )
+
+
+
+class CustomRefreshToken(CustomAbstractRefreshToken):
+    class Meta(AbstractRefreshToken.Meta):
+        swappable = "OAUTH2_PROVIDER_REFRESH_TOKEN_MODEL"
+
+class CustomAbstractIDToken(AbstractIDToken):
+    user = models.ForeignKey(
+        Company,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name="%(app_label)s_%(class)s",
+    )
+
+class CustomIDToken(CustomAbstractIDToken):
+    class Meta(CustomAbstractIDToken.Meta):
+        swappable = "OAUTH2_PROVIDER_ID_TOKEN_MODEL"
+
+
+class MyApplication(AbstractApplication):
+    pass
