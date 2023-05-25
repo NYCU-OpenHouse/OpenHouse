89edb962fa261f7fe86581568de7f5dc5db97d0f
yellowhank <shps97040608@gmail.com>
2023-05-25 17:59:59 +0800

    feat: reject company_visit sign up when deadline reach

--- OpenHouse/company_visit/views.py
+++ OpenHouse/company_visit/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,get_object_or_404
 from .models import CompanyVisit
 from .forms import StudentApplyForm
 from datetime import datetime
+from django.utils import timezone
+
 def company_visit_index(request):
     events = CompanyVisit.objects.all().order_by('date')
     events = [event for event in events if event.date > datetime.today().date()]
@@ -16,6 +18,9 @@ def company_visit_apply(request,id):
     event = get_object_or_404(CompanyVisit,id=id)
     init_data={'event': event}
     form = StudentApplyForm(initial=init_data)
+    if event.signup_deadline < timezone.now().date():
+        return render(request,'visit/company_visit_fail.html',locals())
+    
     if event.get_people_num() >= event.limit:
         message = True
     if(request.method=='POST'):
