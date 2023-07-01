0f90e6e60b734633e2248a74ab92f8ce42b4d522
yellowhank <shps97040608@gmail.com>
2023-07-01 21:15:55 +0800

    fix: company may changed name but still use the same comapny id

--- OpenHouse/company/get_chinese_funded_company/get_chinese_funded_company.py
+++ OpenHouse/company/get_chinese_funded_company/get_chinese_funded_company.py
@@ -18,7 +18,14 @@ str_file = io.StringIO(csv_bytes.decode('utf-8'), newline='\n')
 reader = csv.reader(str_file)
 next(reader)
 for row_list in reader:
-    ChineseFundedCompany.objects.get_or_create(cid=row_list[1], name=row_list[2])
+    try:
+        chinese_funded_compnay = ChineseFundedCompany.objects.get(cid=row_list[1])
+        # chinese funded comapny may only change name 
+        if chinese_funded_compnay.name != row_list[2]:
+            chinese_funded_compnay.name = row_list[2]
+            chinese_funded_compnay.save()
+    except ChineseFundedCompany.DoesNotExist:
+        ChineseFundedCompany.objects.create(cid=row_list[1], name=row_list[2])
 
 # Once the file is closed,
 # any operation on the file (e.g. reading or writing) will raise a ValueError
