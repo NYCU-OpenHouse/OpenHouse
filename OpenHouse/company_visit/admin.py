6f67c772b2cb7d11a3ed420d74aa67c7f277bdbb
tzuhsun <hsungrass@gmail.com>
2024-01-10 22:01:40 +0800

    fix: ckeditor admin redirect issue

--- OpenHouse/company_visit/admin.py
+++ OpenHouse/company_visit/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from . import models
 
+# class CompanyVisitAdmin(admin.ModelAdmin):
+#     pass
+# admin.site.register(models.CompanyVisit, CompanyVisitAdmin)
+
+@admin.register(models.CompanyVisit)
 class CompanyVisitAdmin(admin.ModelAdmin):
     pass
-admin.site.register(models.CompanyVisit, CompanyVisitAdmin)
 
 class StudentApplyAdmin(admin.ModelAdmin):
     list_display = ('event','name','date','SSN','mobile','email','student_id','department','gender','country')
