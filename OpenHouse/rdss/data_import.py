670921275b7e94fd39553125dc177170f7ab480b
tzuhsun <hsungrass@gmail.com>
2025-02-10 17:42:06 +0800

    Feat(recruit): add student import column

--- OpenHouse/rdss/data_import.py
+++ OpenHouse/rdss/data_import.py
@@ -9,8 +9,8 @@ def ImportStudentCardID(file):
 
     for i in range(2, sheet.max_row+1):
         Student.objects.get_or_create(
-            idcard_no= sheet.cell(row=i, column=2).value, 
-            student_id=sheet.cell(row=i, column=1).value, 
+            idcard_no= sheet.cell(row=i, column=2).value,
+            student_id=sheet.cell(row=i, column=1).value,
             name=sheet.cell(row=i, column=3).value,
             email=sheet.cell(row=i, column=4).value or '',
             phone=sheet.cell(row=i, column=5).value or '',
